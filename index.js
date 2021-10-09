b08dbc09d622c3cd26d7e7a9ba0a46504b430499
h3samayoa <h3samayoa@gmail.com>
2021-10-09 11:39:01 -0700

    added boilerplate index file

---
+++ index.js
@@ -0,0 +1,83 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+
+const intro = [
+    {
+        type: 'confirm',
+        prefix: '\b',
+        name: 'intro',
+        message: chalk.magenta(`Welcome to my ReadMe Generator`)
+    },
+];
+
+const successMsg = chalk.greenBright(`The readme has been generated in the output folder!`);
+
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the title of your project:',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your github username:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter a valid email address:',
+        validate: function(value) {
+            let valid = value.match(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
+            if (valid) {
+                return true;
+            } else {
+                return 'Please enter a valid email address';
+            }
+        },
+
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a description of your project:',
+    },
+    {
+        type: 'confirm',
+        name: 'installConf',
+        message: 'Do you want to add installation instructions?'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Add your installation instructions:',
+    },
+    {
+        type: 'confirm',
+        name: 'contribConf',
+        message: 'Do you want to add contribution instructions?'
+    },
+    {
+        type: 'input',
+        name: 'contrib',
+        message: 'Add how to contribute to your repo:',
+    },
+    {
+        type: 'confirm',
+        name: 'testConf',
+        message: 'Do you want to add testing instructions?'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Add your testing instructions:'
+    },
+    {
+        type: 'rawlist',
+        name: 'license',
+        message: 'Choose an open source license:',
+        choices: ['example1', 'example2'],
+    },
+
+]
